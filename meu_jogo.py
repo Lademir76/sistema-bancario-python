3261509fb19d01944ff982390d690d931c198ef8
Lademir Bechara <82599222+Lademir76@users.noreply.github.com>
2025-07-15 02:49:22 -0300

    Delete meu_jogo.py

--- meu_jogo.py
+++
@@ -1,9 +0,0 @@
-import random
-
-numero_secreto = random.randint(1, 10)
-tentativa = int(input("Adivinhe um número entre 1 e 10: "))
-
-if tentativa == numero_secreto:
-    print("Parabéns! Você acertou!")
-else:
-    print(f"Não foi dessa vez. O número era {numero_secreto}.")
